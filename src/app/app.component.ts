21f6082b3cc9b034770da19a46e7a3187cba4328
Korolczuk <alkariandev@gmail.com>
2022-09-26 11:49:41 +0200

    init to azure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AccountService } from './services/account.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'AllergicUI';
+  title = 'Login System';
   users: any;
 
 
